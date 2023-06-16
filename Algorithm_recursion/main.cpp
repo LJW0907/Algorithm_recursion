996ee919e83e9d32e7b03c58222403fcf3debb11
LJW0907 <jwlee0109@gmail.com>
2023-06-16 19:30:15 +0900

    최대공약수 구현

--- Algorithm_recursion/main.cpp
+++ Algorithm_recursion/main.cpp
@@ -22,7 +22,15 @@ int fibo(int n) { //n번째 피보나치 수를 반환한다(n은 양수)
 	return fibo(n - 1) + fibo(n - 2);
 }
 
+int gcd(int m, int n) { //m과 n의 최대공약수 구하기
+	if (n == 0)
+		return m;
+	else
+		return gcd(n, m%n);
+}
+
 int main() {
+	//0~n까지 덧셈
 	/*{
 		int n = 10;
 		int result = 0;
@@ -32,14 +40,23 @@ int main() {
 		cout << result << endl;
 	}*/
 
+	//팩토리얼
 	/*{
 		int result = fact(5);
 
 		cout << result << endl;
 	}*/
 
-	{
+	//피보나치
+	/*{
 		int result = fibo(10);
+
+		cout << result << endl;
+	}*/
+
+	//최대공약수
+	{
+		int result = gcd(4, 10);
 
 		cout << result << endl;
 	}
